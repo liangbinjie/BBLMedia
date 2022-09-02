f78957a1a4162b78ea50c1162d4e806b26f93da3
liangbinjie <mailandresliang@gmail.com>
2022-09-02 11:20:02 -0600

    Created models

--- bllmedia/app/admin.py
+++ bllmedia/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 # Register your models here.
+from .models import Contacto
+
+
+class ContactFiltering(admin.ModelAdmin):
+    list_display = ['email']
+
+
+admin.site.register(Contacto, ContactFiltering)
+admin.site.unregister(Group)
--- bllmedia/app/models.py
+++ bllmedia/app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    email = models.CharField(max_length=400)
